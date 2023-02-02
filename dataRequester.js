a8c2a051d5a1b798fc5831865cdf9e6cb3441a40
Zach <zacharyross1234@gmail.com>
2023-02-02 09:50:37 -0500

    Tomorrow.io API request

--- dataRequester.js
+++ dataRequester.js
@@ -1,6 +1,23 @@
 
+const requestPollenToronto = (() => {
 
+    let key = process.env.TOMORROW_IO_KEY
+    let location = "43.6532,-79.3832" // Toronto
+
+    fetch(`https://api.tomorrow.io/v4/timelines?location=${location}&fields=grassIndex,treeIndex,weedIndex&startTime=nowMinus1h&endTime=now&units=metric&apikey=${key}`).then((response) => {
+        response.json().then((json) => {
+            console.log(JSON.stringify(json))
+
+            // Pass json to the data cache here
+
+        }).catch((err) => {
+            console.log("Unable read JSON -", err)
+        })
+    }).catch((err) => {
+        console.log("Unable to fetch -", err)
+    })
+})
 
 module.exports = {
-    
+    requestPollenToronto
 }
