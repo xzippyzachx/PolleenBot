b4beb944375349bc88421f31fb1e94f4a82e5877
Zach <zacharyross1234@gmail.com>
2023-02-04 13:23:43 -0500

    Tesing Scheduler

--- scheduler.js
+++ scheduler.js
@@ -2,11 +2,14 @@ const poster = require('./poster')
 const dataCache = require('./dataCache')
 const dataRequester = require('./dataRequester')
 
-const startScheduler = (() => {
-    console.log("Post scheduler has started!")
+const cron = require('node-cron');
 
-    
-    // To Do: Scheduler logic...
+const startScheduler = (() => {
+    console.log("Scheduler has started!")
+
+    let dailyTask = cron.schedule("0 6 * * *", () => { // Called daily at 6 AM
+        console.log(`Running a task at [${new Date().toISOString()}]`)
+    })
 })
 
 module.exports = {
