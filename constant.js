d29e27f1fbe645d6ad5d8ccc457f71b399b4724e
Yongjie Ba <baye0110@gmail.com>
2023-02-22 22:07:29 -0500

    upgrade color for post.svg

--- constant.js
+++ constant.js
@@ -2,8 +2,8 @@ const yellowColor = "stop-color:#ffdb1e;stop-opacity:1;";
 const redColor = "stop-color:#ff0000;stop-opacity:1;";
 const greenColor = "stop-color:#6fcf0a;stop-opacity:1;";
 
-const treeValue = 1;
-const grassValue = 1;
-const weedsValue = 1;
+const treeValue = 3;
+const grassValue = 3;
+const weedsValue = 3;
 
 module.exports = {yellowColor,redColor,greenColor,treeValue,grassValue,weedsValue};
--- contentGenerator.js
+++ contentGenerator.js
@@ -1,5 +1,10 @@
 const dataCache = require('./dataCache')
+// const {redColor,yellowColor,greenColor} = require('./constant.js')
 
+// console.log(dataCache.fetch());
+// console.log(redColor);
+// console.log(yellowColor);
+// console.log(greenColor);
 const generate = ( () => {
     console.log("dummy content generation")
 
